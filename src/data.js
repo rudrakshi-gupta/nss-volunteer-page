442fd4569f9ae403d2e67cf15d38bd2657e2300f
Rudrakshi Gupta <109859631+rudrakshi-gupta@users.noreply.github.com>
2023-02-09 22:05:20 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,9 +3,9 @@ const data = {
         {
             "id":1,
             "image":"https://images.unsplash.com/photo-1519819286236-0b3c6506e475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
-            "Name":"Rudrakshi Gupta",
-            "Position":"Editor",
-            "Team" : "Editorial Team",
+            "Name":"Ayushi Agarwal",
+            "Position":"President",
+            "Team" : "Presidental Body",
             "Facebook": "#",
             "Instagram": "#",
             "Twitter":"#"
@@ -13,9 +13,9 @@ const data = {
         {
             "id":2,
             "image":"https://images.unsplash.com/photo-1519819286236-0b3c6506e475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
-            "Name":"Name 2",
-            "Position":"Developer",
-            "Team" : "Techinal Team",
+            "Name":"Tanmay Singhal",
+            "Position":"Vice-President",
+            "Team" : "Presidental Body",
             "Facebook": "#",
             "Instagram": "#",
             "Twitter":"#"
