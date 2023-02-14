46043ff7076a759cfe99e63711c6f9dc7f9a3cb5
Rudrakshi Gupta <109859631+rudrakshi-gupta@users.noreply.github.com>
2023-02-14 20:43:25 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -23,9 +23,9 @@ const data = {
         {
             "id":3,
             "image":"https://images.unsplash.com/photo-1519819286236-0b3c6506e475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
-            "Name":"Name 3",
-            "Position":"Dancer",
-            "Team" : "Cultural Team",
+            "Name":"Nishant Belwal",
+            "Position":"Vice-President",
+            "Team" : "Presidential Body",
             "Facebook": "#",
             "Instagram": "#",
             "Twitter":"#"
@@ -33,9 +33,9 @@ const data = {
         {
             "id":4,
             "image":"https://images.unsplash.com/photo-1519819286236-0b3c6506e475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
-            "Name":"Name 4",
-            "Position":"Editor",
-            "Team" : "Editorial Team",
+            "Name":"Lavanya Pundir",
+            "Position":"Secretary",
+            "Team" : "Presedinial Body",
             "Facebook": "#",
             "Instagram": "#",
             "Twitter":"#"
@@ -43,9 +43,9 @@ const data = {
         {
             "id":1,
             "image":"https://images.unsplash.com/photo-1519819286236-0b3c6506e475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
-            "Name":"Name 5",
-            "Position":"President",
-            "Team" : "-",
+            "Name":"Hiranymayi",
+            "Position":"Genral Secretary",
+            "Team" : "Presidential Body",
             "Facebook": "#",
             "Instagram": "#",
             "Twitter":"#"
@@ -53,9 +53,9 @@ const data = {
         {
             "id":1,
             "image":"https://images.unsplash.com/photo-1519819286236-0b3c6506e475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
-            "Name":"Name 6",
-            "Position":"Developer",
-            "Team" : "Techinical Team",
+            "Name":"Manish Kaushik",
+            "Position":"Treasurer",
+            "Team" : "Presidential Body",
             "Facebook": "#",
             "Instagram": "#",
             "Twitter":"#"
